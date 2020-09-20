867fc74395b46a7d0ace36e2f64bd96ffc535028
Brandon <brandonob@berkeley.edu>
2020-09-20 02:38:54 -0700

    i think im close

--- functions/weather/weather.js
+++ functions/weather/weather.js
@@ -1,7 +1,13 @@
-exports.handler = async (_, _) => {
+exports.handler = async (event, _) => {
   try {
+    console.log(event);
     console.log("testing");
     const { LAT, LON, API_KEY } = process.env;
+    console.log(process);
+    console.log(process.env);
+    console.log(process.env.LAT);
+    console.log(process.env.LON);
+    console.log(process.env.API_KEY);
     let response = await fetch(`https://api.openweathermap.org/data/2.5/oncecall?units=imperial&exclude=minutely&lat=${LAT}&lon=${LON}&appid=${API_KEY}`);
     let data = response.json();
     return { statusCode: 200, body: JSON.stringify(data) };
