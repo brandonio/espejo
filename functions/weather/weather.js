d41d4743ae1d67b1679d726790d91d03558ef851
Brandon <brandonob@berkeley.edu>
2020-09-20 12:14:37 -0700

    i think i finally got it

--- functions/weather/weather.js
+++ functions/weather/weather.js
@@ -1,22 +1,10 @@
 const fetch = require("node-fetch");
 
-exports.handler = async (event, context, callback) => {
+exports.handler = async () => {
   try {
-    const pass = (body) => {
-      callback(null, { statusCode: 200, body: JSON.stringify(body) });
-    };
-
-    const { LAT, LON, API_KEY } = process.env;
-    const url = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&exclude=minutely&lat=${LAT}&lon=${LON}&appid=${API_KEY}`;
-    console.log(url);
-    let response = await fetch(url);
-    let data = await response.json();
-    pass(data);
+    let data = await fetch(process.env.API_URL).then((res) => res.json());
+    return { statusCode: 200, body: JSON.stringify(data) };
   } catch (e) {
-    let error = {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-    await pass(error);
+    return { statusCode: 500, body: e.toString() }
   }
 };
--- src/weather-api.tsx
+++ src/weather-api.tsx
@@ -1,12 +1,7 @@
 const fetchWeather = async (): Promise<any> =>
-  fetch("/.netlify/functions/weather").then((res) => {
-    console.log("res");
-    console.log(res);
-    console.log(res.body);
-    return new Promise((resolve) => {
-      window.setTimeout(() => resolve(res.body), 1);
-    });
-  });
+  await fetch("/.netlify/functions/weather")
+    .then((res) => res.json())
+    .catch((e) => console.log(e));
 
 const processWeather = (): number => 6;
 
